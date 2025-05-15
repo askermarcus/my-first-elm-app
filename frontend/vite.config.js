0a116e08cb7a54e0584c5359c269a545579ecd50
askermarcus <marcus.asker@pinmeto.com>
2025-05-15 16:26:05 +0200

    add vite and tailwindcss

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import elmPlugin from "vite-plugin-elm";
+
+export default defineConfig({
+  plugins: [elmPlugin()],
+  root: ".",
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 8000,
+  },
+});
